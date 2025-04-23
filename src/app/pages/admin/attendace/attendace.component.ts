a3f18fadc2a9a4c56a392ef2e15ffc399af5086c
Johan Crespo <jcrespo@atlanticqi.com>
2025-04-22 21:26:34 -0500

    Se agrego la funcionalidad de exportar la data a excel

--- src/app/pages/admin/attendace/attendace.component.ts
+++ src/app/pages/admin/attendace/attendace.component.ts
@@ -2,6 +2,7 @@ import { Component, inject } from '@angular/core';
 import { FirestoreService } from '../../../services/firestore.service';
 import { FormsModule } from '@angular/forms';
 import { FilterPipe } from '../../../pipes/filter.pipe';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-attendace',
@@ -61,6 +62,46 @@ export class AttendaceComponent {
     this.listAttendance = null;
   }  
 
+  downloadExcel(date) {
+    if (!this.listAttendance || this.listAttendance.length === 0) {
+      console.error('No hay datos para exportar.');
+      return;
+    }
+
+    // Prepara los datos para el archivo Excel
+    const data = this.listAttendance.map((item: any) => ({
+      Nombre: item.nombre || 'N/A',
+      Telefono: item.telefono || 'N/A',
+      Fecha: item.fecha || 'N/A',
+      Casa3D: item.casaTresD || 'N/A',
+      Asistencia: 'Si',
+    }));
+
+    // Crea la hoja de cálculo
+    const worksheet = XLSX.utils.json_to_sheet(data);
+
+    // Crea el libro de trabajo
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, 'Asistencia-'+date.replace(/\//g, '-'));
+
+    // Genera el archivo Excel
+    const excelBuffer = XLSX.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    // Crea un Blob para descargar el archivo
+    const blob = new Blob([excelBuffer], { type: 'application/octet-stream' });
+    const url = window.URL.createObjectURL(blob);
+
+    // Crea un enlace para descargar el archivo
+    const a = document.createElement('a');
+    a.href = url;
+    let nameFile = date == 'todos' ? 'todos-los-registros-Casa3D' : 'Asistencia-Casa3D-'+date.replace(/\//g, '-');
+    a.download = `${nameFile}.xlsx`;
+    a.click();
+
+    // Limpia el objeto URL
+    window.URL.revokeObjectURL(url);
+    a.remove();
+  }
 
   ngOnDestroy() {
     if (this.suscribeList) {
